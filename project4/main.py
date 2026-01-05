b39d0f115d476131030cfca1d3d9bcc7c2503636
metin <metin@gobindsarvar.ca>
2026-01-05 10:26:36 -0800

    modify

--- project4/main.py
+++ project4/main.py
@@ -9,3 +9,4 @@
 # PAUSE 2. Split everything into variables.
 # Split each step in the previous exercise into a separate variable. One variable called username and one called length.
 # Use the variable username in the len calculation.
+# s;dfgjsfupawjv;lwefjl;m
